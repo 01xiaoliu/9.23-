6d5bbd4e2dabf71f61dd28bfb8aebfb277de5bba
01xiaoliu <113813882+01xiaoliu@users.noreply.github.com>
2022-09-23 18:01:19 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,28 @@
+package xue_Liu;
+
+public class Test {
+/*    public static void main(String[] args) {
+        int []c ={81,81,3276,65306,49,48,52,50,57,52,51,52,55,65292,23494,30721,65306,97,98,105,110,103};
+        for(int i:c){
+            System.out.println((char) i);
+        }
+    }*/
+
+    /*public static void main(String[] args) {
+        String s1 = "runoob";
+        String s2 = "runoob";
+        System.out.println("s1 == s2 is:" + s1 ==s2 ); //faolt因为在 Java 中 + 操作符的优先级大于 ==，所以输出部分表达式等于
+        //“s1 == s2 is:runoob” == “runoob”，该表达式计算结果为 false。
+    }*/
+
+/*
+        public static void main(String args[]) {
+            int x=4;
+            System.out.println(x++);//x++ 先输出在自加，++x则反之。
+        }
+*/
+
+
+
+}
+
